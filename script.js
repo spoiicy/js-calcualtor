b181e794f742dbf4b5abe6b7eea6fedf3ad0ec84
spoiicy <akshitmaheshwary@gmail.com>
2023-02-04 09:52:28 +0530

    wrote working functionalityof calculator with some bugs

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function add(num1,num2){
-    console.log(num1+num2);
     return num1+num2;
 }
 
@@ -9,7 +8,6 @@ function subtract(num1,num2){
 }
 
 function mulitply(num1,num2){
-    console.log(num1*num2);
     return num1*num2;
 }
 
@@ -17,7 +15,6 @@ function divide(num1,num2){
     if(num2===0){
         alert('undefined')
     }
-    console.log(num1/num2);
     return num1/num2;
 }
 
@@ -27,75 +24,156 @@ function divide(num1,num2){
 function operate(operator,num1,num2){
     switch(operator){
         case '+':
-            add(num1,num2);
-            break;
+            return add(num1,num2);
         case '-':
-            subtract(num1,num2);
-            break;
+            return subtract(num1,num2);
         case '*':
-            mulitply(num1,num2);
-            break;
+            return mulitply(num1,num2);
         case '/':
-            divide(num1,num2);
-            break;
+            return divide(num1,num2);
     }
 }
 
 
 
+function calculate(){
+
+}
+
+let firstOperator = true;
+let firstOperator_symbol;
+
 function displayNumber(button_id){
     const display_number = document.querySelector('.screen');
-    
     const content = document.createElement('div');
     content.classList.add('screen-content');
     const remove_content = document.querySelectorAll('.screen-content');
     switch(button_id){
         case 'one':
-            content.textContent = '1';
-            display_number.appendChild(content);
+            display_number.textContent += '1';
+            //display_number.appendChild(content);
             break;
         case 'two':
-            content.textContent = '2';
-            display_number.appendChild(content);
+            display_number.textContent += '2';
+            //display_number.appendChild(content);
             break;
         case 'three':
-            content.textContent = '3';
-            display_number.appendChild(content);
+            display_number.textContent += '3';
+            //display_number.appendChild(content);
             break;
         case 'four':
-            content.textContent = '4';
-            display_number.appendChild(content);
+            display_number.textContent += '4';
+            //display_number.appendChild(content);
             break;
         case 'five':
-            content.textContent = '5';
-            display_number.appendChild(content);
+            display_number.textContent += '5';
+            //display_number.appendChild(content);
             break;
         case 'six':
-            content.textContent = '6';
-            display_number.appendChild(content);
+            display_number.textContent += '6';
+            //display_number.appendChild(content);
             break;
         case 'seven':
-            content.textContent = '7';
-            display_number.appendChild(content);
+            display_number.textContent += '7';
+            //display_number.appendChild(content);
             break;
         case 'eight':
-            content.textContent = '8';
-            display_number.appendChild(content);
+            display_number.textContent += '8';
+            //isplay_number.appendChild(content);
             break;
         case 'nine':
-            content.textContent = '9';
-            display_number.appendChild(content);
+            display_number.textContent += '9';
+            //display_number.appendChild(content);
             break;
         case 'zero':
-            content.textContent = '0';
-            display_number.appendChild(content);
+            display_number.textContent += '0';
+            //display_number.appendChild(content);
             break;
         case 'clear':
             display_number.removeChild(display_number.lastChild);
             break;
+        case 'add':
+            if(firstOperator == false){
+                string = document.querySelector('.screen').textContent;
+                string = string.split(firstOperator_symbol);
+                let result = operate(firstOperator_symbol,parseInt(string[0]), parseInt(string[1]));
+                display_number.textContent = '';
+                display_number.textContent = result + '+';
+                firstOperator_symbol = '+';
+                console.log(result);
+            }
+            else{
+                firstOperator_symbol = '+';
+                display_number.textContent += '+';
+                firstOperator = false;
+            }
+            
+            //display_number.appendChild(content);
+            calculate();
+            break;
+    
+        
+        case 'multiply':
+            if(firstOperator == false){
+                string = document.querySelector('.screen').textContent;
+                string = string.split(firstOperator_symbol);
+                let result = operate(firstOperator_symbol,parseInt(string[0]), parseInt(string[1]));
+                console.log(result);
+                display_number.textContent = '';
+                display_number.textContent = result+'*';
+                firstOperator_symbol = '*';
+                
+            }
+            else{
+                firstOperator_symbol = '*';
+                display_number.textContent += '*';
+                firstOperator = false;
+            }
+            break;
+            
+        
+        case 'subtract':
+            if(firstOperator == false){
+                string = document.querySelector('.screen').textContent;
+                string = string.split(firstOperator_symbol);
+                let result = operate(firstOperator_symbol,parseInt(string[0]), parseInt(string[1]));
+                console.log(result);
+                display_number.textContent = '';
+                display_number.textContent = result+'-';
+                firstOperator_symbol = '-';
+                
+            }
+            else{
+                firstOperator_symbol = '-';
+                display_number.textContent += '-';
+                firstOperator = false;
+            }
+            break;
+        case 'divide':
+            if(firstOperator == false){
+                string = document.querySelector('.screen').textContent;
+                string = string.split(firstOperator_symbol);
+                let result = operate(firstOperator_symbol,parseInt(string[0]), parseInt(string[1]));
+                console.log(result);
+                display_number.textContent = '';
+                display_number.textContent = result+'/';
+                firstOperator_symbol = '/';
+                
+            }
+            else{
+                firstOperator_symbol = '/';
+                display_number.textContent += '/';
+                firstOperator = false;
+            }
+            break;
+            
+            
+
         case 'ac':
             remove_content.forEach(element => element.remove());
             break;
+
+        
 
     }
     
@@ -113,4 +191,6 @@ buttons.forEach((button) => {
 
 
 
-operate('*',1,2);
+
+
+
